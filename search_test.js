787c717525e3ec31c9b510a93a03e5647c883613
Bharat Goyal <ashitaatri@Ashitas-MacBook-Air.local>
2023-12-13 19:32:39 +0000

    search test

---
+++ search_test.js
@@ -0,0 +1,15 @@
+const assert = require("assert");
+
+Feature('search');
+
+Data(['Myst','Matt']).Scenario('partial text search should only display matching results', async ({ I,current}) => {
+    I.amOnPage('/');
+    I.fillField('.toolbar-search-input',current);
+    I.click('.toolbar-search-button')
+    I.scrollPageToBottom();
+    
+    const searchResults = await I.grabTextFromAll('.thumb-data-item--name-container')
+    searchResults.forEach((text) => {
+        I.assertContain(text.toLocaleLowerCase(),current.toLocaleLowerCase());
+    });
+});
